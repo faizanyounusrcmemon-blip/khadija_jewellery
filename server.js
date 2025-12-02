9d7380f63ec7a22a5870f8e0d1538799cdbff012
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2025-12-02 16:08:21 +0500

    Update server.js

--- server.js
+++ server.js
@@ -214,7 +214,7 @@ app.post("/api/archive-delete", async (req, res) => {
 });
 
 // =====================================================================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("🚀 Server running on port " + PORT);
 });
