3e043776b6866df0683c08cbaf6e9b8779481bd0
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2025-12-03 13:47:46 +0500

    Update archive-transfer.js

--- archive-transfer.js
+++ archive-transfer.js
@@ -6,21 +6,27 @@ app.post("/api/archive-transfer", async (req, res) => {
       return res.json({ success: false, error: "Wrong password" });
 
     const sql = `
-      INSERT INTO archive (barcode, item_name, purchase_qty, sale_qty, return_qty, created_at)
+      INSERT INTO archive (barcode, item_name, purchase_qty, sale_qty, return_qty, date, created_at)
       SELECT 
         barcode,
         item_name,
         purchase_qty,
         sale_qty,
         return_qty,
+        $1::date,     -- archive date will be the fromDate (or you can use endDate)
         NOW()
       FROM summary_view
-      WHERE final_date BETWEEN $1 AND $2;
+      WHERE final_date IS NOT NULL;
     `;
 
-    await pg.query(sql, [start_date, end_date]);
+    const result = await pg.query(sql, [start_date]);
 
-    res.json({ success: true, message: "Data transferred to archive!" });
+    res.json({
+      success: true,
+      message: "Data transferred to archive!",
+      transferred_rows: result.rowCount
+    });
+
   } catch (err) {
     res.json({ success: false, error: err.message });
   }
